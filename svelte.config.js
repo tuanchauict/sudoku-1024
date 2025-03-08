87ee2695cb1375fd05fd858f525d3599d44d33a0
Tuan Chau (Tuna) <tuan@iamtuna.org>
2025-03-08 18:12:02 +0900

    Fix config

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			precompress: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
+			base: process.env.NODE_ENV === 'production' ? '/sudoku-1024' : ''
 		}
 	}
 };
