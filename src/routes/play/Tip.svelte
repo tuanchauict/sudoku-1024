f9b7fcb1070c48223e2062f5aae16672c40139e2
Tuan Chau (Tuna) <tuan@iamtuna.org>
2025-03-09 18:33:02 +0900

    Minor update

--- src/routes/play/Tip.svelte
+++ src/routes/play/Tip.svelte
@@ -2,14 +2,15 @@
 	Tip: Select a cell and press 1-9 to enter values. Use note mode for candidates.
 </div>
 <div class="tip">
-	Any issue or suggestion? Please report on <a href="https://github.com/tuanchauict/sudoku-1024/">GitHub</a> of the project.
+	Any issue or suggestion? Please report on <a href="https://github.com/tuanchauict/sudoku-1024/"
+																							 target="_blank">GitHub</a> of the project.
 </div>
 
 <style>
-	.tip {
-		font-size: 14px;
-		color: #666;
-		margin-bottom: 12px;
-		text-align: center;
-	}
+    .tip {
+        font-size: 14px;
+        color: #666;
+        margin-bottom: 12px;
+        text-align: center;
+    }
 </style>
