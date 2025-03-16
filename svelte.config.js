6b492a41fb24871694789c155d06f44af00b512c
Tuan Chau (Tuna) <tuan@iamtuna.org>
2025-03-16 16:29:36 +0900

    Prepare for release

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,14 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sudoku-1024' : ''
+			base: process.env.NODE_ENV === 'production' ? '' : ''
 		},
 		appDir: 'app'
 	}
